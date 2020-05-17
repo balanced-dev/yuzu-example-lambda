0e9e53ccc25ca50fd3f78cf0f2ac8fa810b6db35
Phil Whittaker <phil@hifi.agency>
2020-05-17 07:37:24 +0100

    upgrade to latest yuzu

--- delivery.src/Lambda/App_Plugins/YuzuDeliveryUmbracoImport/licenseDialog/licenseDialog.controller.js
+++ delivery.src/Lambda/App_Plugins/YuzuDeliveryUmbracoImport/licenseDialog/licenseDialog.controller.js
@@ -1,6 +1,6 @@
 ﻿var app = angular.module("umbraco");
 
-function licenseDialog($scope, formHelper, yuzuImportResources) {
+function licenseDialog($scope, formHelper, notificationsService, yuzuImportResources) {
 
     var vm = this;
 
@@ -15,7 +15,20 @@ function licenseDialog($scope, formHelper, yuzuImportResources) {
             yuzuImportResources.activateLicense(vm.data)
                 .then(function (response) {
                     vm.licenseStatus = response.data.licenseStatus;
-                    vm.licenseMessage = response.data.licenseMessage;
+                    vm.licenseName = response.data.licenseName;
+                    vm.licenseEmail = response.data.licenseEmail;
+                    vm.licenseKey = response.data.licenseKey;
+
+                    if (vm.licenseStatus === 2) {
+                        vm.buttonState = "success";
+                        notificationsService.success("License Activated");
+                        $scope.model.close();
+                    }
+                    else {
+                        vm.buttonState = "error";
+                        notificationsService.error("License Activation Error", response.data.licenseMessage);
+                    }
+
                 });
         }
 
@@ -24,10 +37,23 @@ function licenseDialog($scope, formHelper, yuzuImportResources) {
     vm.close = $scope.model.close;
 
     vm.deactivateLicense = function () {
+
+        vm.buttonState = "busy";
+
         yuzuImportResources.deactivateLicense()
             .then(function (response) {
                 vm.licenseStatus = response.data.licenseStatus;
                 vm.licenseMessage = response.data.licenseMessage;
+
+                if (response.data.licenseStatus === 2) {
+                    vm.buttonState = "success";
+                    notificationsService.error("License Deactivation Error", response.data.licenseMessage);
+                }
+                else {
+                    vm.buttonState = "success";
+                    notificationsService.success("License Deactivated");
+                }
+
             });
     };
 
@@ -35,6 +61,9 @@ function licenseDialog($scope, formHelper, yuzuImportResources) {
         .then(function (response) {
             vm.licenseStatus = response.data.licenseStatus;
             vm.licenseMessage = response.data.licenseMessage;
+            vm.licenseName = response.data.licenseName;
+            vm.licenseEmail = response.data.licenseEmail;
+            vm.licenseKey = response.data.licenseKey;
         });
 
     vm.data = {};
